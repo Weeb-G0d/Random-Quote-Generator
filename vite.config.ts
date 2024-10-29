8a9cbc61fca589eef128e03e29a85b41e15aa313
Weeb-G0d <maharjanbichheach0909@gmail.com>
2024-10-30 02:31:12 +0545

    Fix imports for react-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   build: {
     rollupOptions: {
-      external: ['react-icons/fa'], // Add this line to handle react-icons
+      external: ['react-icons/fa'], // Add this line
     },
   },
 });
-
