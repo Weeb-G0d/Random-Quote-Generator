1ad1fe49e918b470e6aac01efef56323dd1da6d4
Weeb-G0d <69193878+Weeb-G0d@users.noreply.github.com>
2024-10-30 01:44:49 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'] // Add the external dependency here
-    }
-  }
 })
